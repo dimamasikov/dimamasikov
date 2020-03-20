da234c73fe9f20410a82f97efd0dcf541c1ff8ae
dimamasikov <62286119+dimamasikov@users.noreply.github.com>
2020-03-20 23:53:55 +0700

    Update script.js

--- regular_expressions/lesson3/script.js
+++ regular_expressions/lesson3/script.js
@@ -1,1 +1,7 @@
+let str = '31.12.2025';
+let a= str.match(/(.+)\.(.+)\.(.+)/);
+console.log("Элемент "+a[0]);
+console.log("День: "+a[1]);
+console.log("Месяц: "+a[2]);
+console.log("Год: "+a[3]);
 
