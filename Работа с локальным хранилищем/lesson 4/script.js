3114750acb3d473bf0fbf1558e21c7783a3ddb90
dimamasikov <62286119+dimamasikov@users.noreply.github.com>
2020-04-09 15:24:07 +0700

    Update script.js

--- Работа с локальным хранилищем/lesson 4/script.js
+++ Работа с локальным хранилищем/lesson 4/script.js
@@ -1,1 +1,15 @@
-
+let textArea1 = document.querySelector('#text1'),
+textArea2 = document.querySelector('#text2'),
+btn1 = document.querySelector('#save'),
+btn2 = document.querySelector('#show'),
+valueArray = [],
+count;
+btn1.addEventListener('click', function (argument) {
+valueArray.push(textArea1.value);
+localStorage.setItem('text', valueArray);
+textArea1.value = '';
+});
+btn2.addEventListener('click', function () {
+let h = localStorage.getItem('text');
+textArea2.value = h;
+});
