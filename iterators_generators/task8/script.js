1621a434f4d45bb972885513cdc19eb23b3454ea
dimamasikov <62286119+dimamasikov@users.noreply.github.com>
2020-04-14 13:28:28 +0700

    Update script.js

--- iterators_generators/task8/script.js
+++ iterators_generators/task8/script.js
@@ -1,1 +1,13 @@
+function * createIterator(arr) {
+while (arr.length) {
+yield console.log(arr.splice(0, 2));
+}
+}
 
+const iterator = createIterator([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+iterator.next();
+iterator.next();
+iterator.next();
+iterator.next();
+iterator.next();
