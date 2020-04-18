773760941847f2646181f7efdaae8ddcaf07eff9
dimamasikov <62286119+dimamasikov@users.noreply.github.com>
2020-04-18 15:36:47 +0700

    Create script.js

---
+++ OOP/lesson 66/task 1/script.js
@@ -0,0 +1,7 @@
+let student = new Student('Дмитрий', 'Масиков', 2018);
+
+console.log(student.name);
+console.log(student.surname);
+console.log(student.getFullName());
+console.log(student.year);
+console.log(student.getCourse());
