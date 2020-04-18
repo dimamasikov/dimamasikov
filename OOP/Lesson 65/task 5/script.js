071a713a7c905881af104137b366e7dac7d24690
dimamasikov <62286119+dimamasikov@users.noreply.github.com>
2020-04-18 14:58:59 +0700

    Create script.js

---
+++ OOP/Lesson 65/task 5/script.js
@@ -0,0 +1,6 @@
+var validator = new Validator();
+
+console.log(validator.isEmail('phphtml@mail.ru'));
+console.log(validator.isDomain('phphtml.net'));
+console.log(validator.isDate('12.05.2020'));
+console.log(validator.isPhone('+375 (29) 817-68-92'));
