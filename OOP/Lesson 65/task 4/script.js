d4ccf1c04be37fe2e3769c712eb8b37e154af7fe
dimamasikov <62286119+dimamasikov@users.noreply.github.com>
2020-04-18 14:45:43 +0700

    Create script.js

---
+++ OOP/Lesson 65/task 4/script.js
@@ -0,0 +1,5 @@
+var str = new MyString();
+
+console.log(str.reverse('abcde')); //выведет 'edcba'
+console.log(str.ucFirst('abcde')); //выведет 'Abcde'
+console.log(str.ucWords('abcde abcde abcde')); //выведет 'Abcde Abcde Abcde'
