2dc9d7b011c35decd2c23b8f4f3bbb955a3bf561
dimamasikov <62286119+dimamasikov@users.noreply.github.com>
2020-04-25 20:46:56 +0700

    Update script.js

--- Фреймворк Vue/lesson 15/task 8/script.js
+++ Фреймворк Vue/lesson 15/task 8/script.js
@@ -1,1 +1,11 @@
-
+let app = new Vue({
+	el: '#app',
+	data: {
+		date: '2020-04-25',
+	},
+	filters: {
+		деньмесяцгод: function(value){
+			return value.split('-').reverse().join('.');
+		}
+	}
+});
